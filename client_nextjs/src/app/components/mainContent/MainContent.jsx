2e57ff03ee0c03989386a0f8707acb1d325fb398
Kunwar-bir-singh <kunwar.bir21@gmail.com>
2024-03-16 10:27:36 +0530

    CHANGES

--- client_nextjs/src/app/components/mainContent/MainContent.jsx
+++ client_nextjs/src/app/components/mainContent/MainContent.jsx
@@ -17,14 +17,34 @@ const MainContent = () => {
     const getSearch = ()=>{
         console.log(input);
     }
+
+    const citiesData = async()=>{
+        const api = await fetch('https://raw.githubusercontent.com/fayazara/Indian-Cities-API/master/cities.json');
+        const data = await api.json();
+        return data;
+    }   
+    citiesData().then((data)=>{
+        const city= data.cities[0];
+        console.log(city);
+    }).catch((error)=>{
+        console.log(error);
+    })
   return (
     <>
     <div>
         <input className='p-1 m-1 border-2 border-slate-950	 text-lg' type="text" name="profession" onChange={getInput} value={input.profession} id="" />
         <input className='p-1 m-1 border-2 border-slate-950	 text-lg' type="text" name="city" onChange={getInput} value={input.city} id="" />
-        <button onClick={getSearch} >Search</button>
+        <button className='p-2 m-4 bg-pink-800 text-lg text-white rounded-lg ' onClick={getSearch} >Search</button>
     </div>
-    
+    <table>
+        <tbody>
+
+        <tr>
+            <th>City</th>
+            <th>State</th>
+        </tr>
+        </tbody>
+    </table>
     </>
 
   )
