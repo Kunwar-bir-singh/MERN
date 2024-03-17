259b74a382df3956556acc943698788782fa6961
Kunwar-bir-singh <kunwar.bir21@gmail.com>
2024-03-17 10:29:32 +0530

    .

--- client_nextjs/src/app/components/mainContent/MainContent.jsx
+++ client_nextjs/src/app/components/mainContent/MainContent.jsx
@@ -1,65 +1,95 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const MainContent = () => {
-    const [input, setInput] = useState({
-        profession:"",
-        city:""
-    })
+  const [res, setRes] = useState([]);
+  const citiesData = async () => {
+    const api = await fetch(
+      "https://raw.githubusercontent.com/fayazara/Indian-Cities-API/master/cities.json"
+    );
+    const data = await api.json();
+    setRes(data.cities);
+  };
 
-    const [city, setCity] = useState([])
+  const [input, setInput] = useState({
+    profession: "",
+    city: "",
+  });
 
-    const getInput =(e)=>{
-        const {name ,value} = e.target;
-         
-        setInput({
-            ...input ,
-            [name] : value
-        })
-    }
-
+  const getInput = (e) => {
+    const { name, value } = e.target;
     
-    const citiesData = async()=>{
-        const api = await fetch('https://raw.githubusercontent.com/fayazara/Indian-Cities-API/master/cities.json');
-        const data = await api.json();
-        return data;
-    }   
-    const getSearch = ()=>{
-        console.log(input);
-        citiesData().then((data)=>{
-            setCity(data.cities)
-        //     city.map((item , index)=>{
-        //     if(item.City.toLowerCase()== input.city.toLowerCase()){
-        //         console.log("The City Is Found At Index : ",index);
-        //     }
-        //     else{
-        //         console.log("No Such City Was Found");
-        //     }
-        // })
-        }).catch((error)=>{
-            console.log(error);
-        })
-        
-    }
-console.log(city);  
-return (
-    <>
-    <div>
-        <input className='p-1 m-1 border-2 border-slate-950	 text-lg' type="text" name="profession" onChange={getInput} value={input.profession} id="" />
-        <input className='p-1 m-1 border-2 border-slate-950	 text-lg' type="text" name="city" onChange={getInput} value={input.city} id="" />
-        <button className='p-2 m-4 bg-pink-800 text-lg text-white rounded-lg ' onClick={getSearch} >Search</button>
-    </div>
-    <table>
-        <tbody>
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
 
-        <tr>
+  useEffect(() => {
+    citiesData();
+  }, []);
+
+  const [displayData , setDisplayData] = useState([]);
+
+  const search = ()=>{
+    const foundCity = res.filter((item , index)=>(
+        item.City.toLowerCase().includes(input.city.toLowerCase())
+    ))
+    console.log(foundCity);
+    setDisplayData[foundCity];
+  }
+
+  return (
+    <>
+      <div>
+        <input
+          className="p-1 m-1 border-2 border-slate-950	 text-lg"
+          type="text"
+          name="profession"
+          onChange={getInput}
+          value={input.profession}
+          id=""
+        />
+        <input
+          className="p-1 m-1 border-2 border-slate-950	 text-lg"
+          type="text"
+          name="city"
+          onChange={getInput}
+          value={input.city}
+          id=""
+        />
+        <button onClick={search} className="p-2 m-4 bg-pink-800 text-lg text-white rounded-lg " >
+          Search
+        </button>
+      </div>
+      <table>
+        <tbody>
+          <tr>
             <th>City</th>
             <th>State</th>
-        </tr>
+          </tr>
+          <>
+            {input ===  "" ? (
+                <h3>Enter The City To Be Searched</h3>
+            )
+            :(
+                <>
+                {displayData.length > 0 ? (
+                    displayData.map((item , index)=>{
+                        <tr>
+                            <td>{displayData.item.City}</td>
+                        </tr>
+                    })
+                ):(
+                    <h3>So Such City Is Found</h3>
+                )}
+                </>
+            )
+            }
+          </>
         </tbody>
-    </table>
+      </table>
     </>
+  );
+};
 
-  )
-}
-
-export default MainContent
+export default MainContent;
